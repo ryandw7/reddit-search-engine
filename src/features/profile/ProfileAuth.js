130bdebe31a153c574a29d77e291342849af2cab
ryandw7 <ryandw77@gmail.com>
2024-05-21 15:21:43 -0400

    add comments to ProfileAuth.js

--- src/features/profile/ProfileAuth.js
+++ src/features/profile/ProfileAuth.js
@@ -5,32 +5,37 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
 export default function ProfileAuth() {
+    
     //view status of fetchAccessToken
     const status = useSelector(selectStatus);
     const signedIn = useSelector(selectSignedIn);
     const dispatch = useDispatch();
-    const w = window.location.href;
-
 
     //pull code and state from url after signing in w/ reddit
+    const w = window.location.href;
     if (w.includes('code=') && !status && sessionStorage.getItem("access_token") === null) {
         let code = w.split('code=')[1];
         code = code.split('#')[0];
         dispatch(fetchAccessToken(code)).then(dispatch(fetchUserInfo())).catch(error => console.log(error));
 
     }
-    useEffect(()=>{
+
+    //seperate sign in from access token retrieval to ensure signedIn will not be set w/o accss token
+    useEffect(() => {
         if (sessionStorage.getItem("access_token")) {
             dispatch(setSignedIn(true));
-            console.log(`Signed In`)
+            console.log(`Signed In`);
         }
-    },[status])
+    }, [status]);
 
+    //set sign in to false and clear out session storage to ensure subscribed subreddits are removed
     const signOut = () => {
         sessionStorage.clear();
         dispatch(setSignedIn(false));
         window.history.replaceState({}, '/', '/');
-    }
+    };
+    
+    //if signed in : render sign out, if signed out : render sign in
     return (
         <div className="profile" data-testid="profile">
             {signedIn ?
