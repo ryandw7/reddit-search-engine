b4cfe24a82fe2de65af5292f8c53d9cf57fe9e3a
ryandw7 <ryandw77@gmail.com>
2024-05-22 12:35:13 -0400

    add thumbnail img and upvotes to listing

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -5,9 +5,10 @@ export default function Listing({ listing }) {
     return (
         <div data-testid="listing" className="listing">
             <h4 className="listing-sr-name">{listing.subreddit_name_prefixed}</h4>
-            <h5 className="listing-author">{listing.author}</h5>
+            <h5 className="listing-author">`u/{listing.author}`</h5>
             <h3 className="listing-title">{listing.title}</h3>
-            <p></p>
+            <img src={listing.thumbnail} height={listing.thumbnail_height} width={listing.thumbnail_width}/>
+            <p>{`^${listing.ups}`}</p>
         </div>
     )
 }
