15ba7bb5f9f828419e7b458ccb534c35d73e3c29
ryandw7 <ryandw77@gmail.com>
2024-05-23 17:22:07 -0400

    added num of comments to listing.js

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -5,10 +5,11 @@ export default function Listing({ listing }) {
     return (
         <div data-testid="listing" className="listing">
             <h4 className="listing-sr-name">{listing.subreddit_name_prefixed}</h4>
-            <h5 className="listing-author">`u/{listing.author}`</h5>
+            <h5 className="listing-author">u/{listing.author}</h5>
             <h3 className="listing-title">{listing.title}</h3>
             <img src={listing.thumbnail} height={listing.thumbnail_height} width={listing.thumbnail_width}/>
-            <p>{`^${listing.ups}`}</p>
+            <p>^{listing.ups} comments: {listing.num_comments}</p>
+
         </div>
     )
 }
