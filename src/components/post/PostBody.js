2b592015faf4b79d59c250d0267cf96d59a1508f
ryandw7 <ryandw77@gmail.com>
2024-05-31 21:20:10 -0400

    reformat PostBody to only show included items

--- src/components/post/PostBody.js
+++ src/components/post/PostBody.js
@@ -1,19 +1,18 @@
 import ReactPlayer from "react-player";
 
-export default function Post({post}){
-    const { permalink, id, imgUrl, hasImg, isVideo, preview, subreddit, author, title, thumbnail, thumbnailHeight, thumbnailWidth, num_comments, ups, secure_media, selftext } = post.body;
+export default function Post({ post }) {
+    const { permalink, id, imgUrl, hasImg, isVideo, preview, subreddit, author, title, url, thumbnailHeight, thumbnailWidth, num_comments, ups, secure_media, selftext } = post.body;
     //render listing and media for search results or home listings
-    const video = secure_media ? secure_media.reddit_video.scrubber_media_url : null;
+    const video = secure_media ? secure_media.reddit_video.fallback_url : null;
     return (
         <div data-testid="post" className="post">
-             <h4 className="listing-sr-name">{subreddit}</h4>
+            <h4 className="listing-sr-name">{subreddit}</h4>
             <h5 className="listing-author">u/{author}</h5>
             <h3 className="listing-title">{title}</h3>
-            {hasImg && <img src={imgUrl} height={thumbnailHeight} width={thumbnailWidth} />}
+            {(url && url.includes('png' || 'jpeg' || 'jpg')) && <img src={url} width="60%"/>}
             <p>{selftext}</p>
-            <p>^{ups} comments: {num_comments}</p>
-            <p></p>
-            <ReactPlayer playing="false" loop="false" controls="true" url={video}></ReactPlayer>
+            {video && <ReactPlayer playing="false" loop="false" controls="true" url={video}></ReactPlayer>}
+            <p>upvotes: {ups} comments: {num_comments}</p>
         </div>
     )
-}
+} 1
