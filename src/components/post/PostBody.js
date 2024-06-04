af3ef09ba3f6ce45185d7bb4ecf40081897ff792
Ryan Wilson <ryandw77@gmail.com>
2024-06-04 17:43:31 +0000

    add dependencies for html-react-parser

--- src/components/post/PostBody.js
+++ src/components/post/PostBody.js
@@ -1,6 +1,7 @@
 import ReactPlayer from "react-player";
-
+import parse from 'html-react-parser';
 export default function Post({ post }) {
+    const test = parse('<div><p>Hello Wolrd</p></div>')
     const { permalink, id, imgUrl, hasImg, isVideo, preview, subreddit, author, title, url, thumbnailHeight, thumbnailWidth, num_comments, ups, secure_media, selftext } = post.body;
     //render listing and media for search results or home listings
     const video = secure_media ? secure_media.reddit_video.fallback_url : null;
@@ -9,6 +10,7 @@ export default function Post({ post }) {
             <h4 className="listing-sr-name">{subreddit}</h4>
             <h5 className="listing-author">u/{author}</h5>
             <h3 className="listing-title">{title}</h3>
+            {test}
             {(url && url.includes('png' || 'jpeg' || 'jpg')) && <img src={url} width="60%"/>}
             <p>{selftext}</p>
             {video && <ReactPlayer playing="false" loop="false" controls="true" url={video}></ReactPlayer>}
