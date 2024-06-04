4ea9a2a3518c926f7a4a8890d471e89393a58fe9
Ryan Wilson <ryandw77@gmail.com>
2024-06-04 20:31:38 +0000

    remove import for HTML parser

--- src/components/post/PostBody.js
+++ src/components/post/PostBody.js
@@ -1,9 +1,8 @@
 import ReactPlayer from "react-player";
-import parse from 'html-react-parser';
 import Markdown from 'react-markdown'
 export default function Post({ post }) {
 
-    const { permalink, id, imgUrl, hasImg, isVideo, preview, subreddit, author, title, url, thumbnailHeight, thumbnailWidth, num_comments, ups, secure_media, selftext } = post.body;
+    const { permalink, id, imgUrl, hasImg, isVideo, preview, subreddit, author, title, url, thumbnailHeight, thumbnailWidth, num_comments, ups, secure_media, selftext, thumbnail } = post.body;
     //render listing and media for search results or home listings
 
     const video = secure_media ? secure_media.reddit_video.fallback_url : null;
@@ -11,8 +10,19 @@ export default function Post({ post }) {
         <div data-testid="post" className="post">
             <h4 className="listing-sr-name">{subreddit}</h4>
             <h5 className="listing-author">u/{author}</h5>
-            <h3 className="listing-title">{title}</h3>
-            {(url && url.includes('png' || 'jpeg' || 'jpg')) && <img src={url} width="60%"/>}
+
+            {(url && url.includes('html'))
+                ?
+                <div className="article-link">
+                    (thumbnail && thumbnail.includes('png' || 'jpeg' || 'jpg')) && <img src={thumbnail} width="20%" />
+                    <h3>{title}</h3>
+                    <a href={url}>{url}</a>
+                </div>
+                :
+                <h3 className="listing-title">{title}</h3>
+            }
+            {(url && url.includes('png' || 'jpeg' || 'jpg')) && <img src={url} width="60%" />}
+
             <Markdown>{selftext}</Markdown>
             {video && <ReactPlayer playing="false" loop="false" controls="true" url={video}></ReactPlayer>}
             <p>upvotes: {ups} comments: {num_comments}</p>
